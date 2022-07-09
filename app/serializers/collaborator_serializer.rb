101dbbbcb9271e89853e004d91d6b7086ba6a290
Brody Losh <brodylosh@gmail.com>
2022-07-08 20:06:21 -0400

    Initial Serialize

--- app/serializers/collaborator_serializer.rb
+++ app/serializers/collaborator_serializer.rb
@@ -1,5 +1,6 @@
 class CollaboratorSerializer < ActiveModel::Serializer
-  attributes :id, :name,:image_url, :address, :remote, :email, :phone_number, :username, :password
+  attributes :id, :name, :image_url, :address, :remote, :email, :phone_number, :username, :password
 
+  has_many :deals
   has_many :athletes, through: :deals
 end
--- app/serializers/deal_serializer.rb
+++ app/serializers/deal_serializer.rb
@@ -1,5 +1,6 @@
 class DealSerializer < ActiveModel::Serializer
   attributes :id, :collaborator_id, :athlete_id, :title, :offer, :description, :criteria, :university, :sport, :year, :age, :gender, :expiration_date, :location, :completed
 
+  belongs_to :athletes
   belongs_to :collaborator
 end
