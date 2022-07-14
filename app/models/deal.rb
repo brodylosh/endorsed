8ff2ca1deb4928b308acfccc6e214518716b4629
Brody Losh <brodylosh@gmail.com>
2022-07-14 18:59:06 -0400

    Validations

--- app/models/deal.rb
+++ app/models/deal.rb
@@ -4,9 +4,9 @@ class Deal < ApplicationRecord
     has_many :applications
     has_many :athletes, through: :applications
 
-    # validates :title, presence: true
-    # validates :description, presence: true
-    # validates :offer, presence: true
-    # validates :expiration_date, presence: true
-    # validates :location, presence: true
+    validates :title, presence: true
+    validates :description, presence: true
+    validates :offer, presence: true
+    validates :expiration_date, presence: true
+    validates :location, presence: true
 end
